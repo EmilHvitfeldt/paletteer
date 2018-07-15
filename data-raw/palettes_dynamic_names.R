4ecfc22e41906ec0cc7409035523090bc271ef61
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2018-07-15 14:22:42 -0700

    fixing column name in palettes_dynamic_names

--- data-raw/palettes_dynamic_names.R
+++ data-raw/palettes_dynamic_names.R
@@ -5,7 +5,7 @@ library(paletteer)
 
 palettes_dynamic_names <- purrr::imap_dfr(palettes_dynamic,
                                           ~ data.frame(package = .y,
-                                                       name = names(.x),
+                                                       palette = names(.x),
                                                        length = sapply(.x, length),
                                                        stringsAsFactors = FALSE)) %>%
   tibble::remove_rownames()
