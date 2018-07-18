86c40697bb656e35247b1de2c8cce47e91386f6b
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2018-07-18 11:30:57 -0700

    fixing miss-classified palettes

--- data-raw/palettes_dynamic_names.R
+++ data-raw/palettes_dynamic_names.R
@@ -3,13 +3,14 @@ library(purrr)
 library(dplyr)
 library(paletteer)
 
-g <- purrr::imap_dfr(palettes_dynamic,
+palettes_dynamic_names <- purrr::imap_dfr(palettes_dynamic,
                                           ~ data.frame(package = .y,
                                                        palette = names(.x),
                                                        length = sapply(.x, length),
                                                        stringsAsFactors = FALSE)) %>%
   tibble::remove_rownames() %>%
-  dplyr::mutate(type = case_when(package == "cartography" ~ "sequential",
+  dplyr::mutate(type = case_when(palette %in% c("pastel.pal", "multi.pal") ~ "qualitative",
+                                 package == "cartography" ~ "sequential",
                                  TRUE ~ "qualitative"))
 
 usethis::use_data(palettes_dynamic_names, overwrite = TRUE)
