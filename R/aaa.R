35baa54df2ddd76e166d645b4bdf0626a9602bdc
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-01-06 15:07:41 -0800

    make check_palette() more informative

--- R/aaa.R
+++ R/aaa.R
@@ -1,7 +1,7 @@
 check_palette <- function(palette, names, call = rlang::caller_env()) {
   if (!(palette %in% names)) {
     cli::cli_abort(
-      "Palette not found. Make sure both package and palette
+      "Palette {.val {palette}} not found. Make sure both package and palette
       name are spelled correct in the format {.val package::palette}.",
       call = call
     )
